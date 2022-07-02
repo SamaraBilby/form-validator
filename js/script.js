8c98ddd89b5d5e35993354685cfe56ecacec2063
SamaraBilby <samarabilby@hotmail.com>
2022-07-02 09:04:34 -0400

    inicio js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const nomeUsuario = document.getElementById("nome-usuario");
+const emailUsuario = document.getElementById("senha-usuario");
+const senha = document.getElementById("senha-usuario");
+const senha2 = document.getElementById("senha2-usuario");
